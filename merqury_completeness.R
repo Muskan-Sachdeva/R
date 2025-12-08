dedfa3b49bcf03265b6f20b37092a675c57d92cd
Muskan-Sachdeva <sachdevamuskan666@gmail.com>
2025-12-08 09:08:09 +0530

    Update merqury_completeness.R

--- merqury_completeness.R
+++ merqury_completeness.R
@@ -2,13 +2,13 @@ library(ggplot2)
 
 # Your data (order will be preserved exactly as written here)
 df <- data.frame(
-  Genotype = factor(c("A", "B", "C", "D", "E"),
-                    levels = c("A", "B", "C", "D", "E")),
+  Genotype = factor(c("HY3C", "MC50", "MC70", "ICPL-13271", "ICPL-87"),
+                    levels = c("HY3C", "MC50", "MC70", "ICPL-13271", "ICPL-87")),
   Completeness = c(95.73, 97.24, 94.96, 88.38, 92.07)
 )
 
 ggplot(df, aes(x = Genotype, y = Completeness)) +
-  geom_bar(stat = "identity", fill = "#2B687C", color = "black", width = 0.55) +  # better blue + narrow width
+  geom_bar(stat = "identity", fill = "#2B687C", width = 0.55) +  # better blue + narrow width
   geom_text(aes(label = Completeness),
             vjust = -0.4, size = 4) +
   labs(
