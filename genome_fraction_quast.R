973da18e1eff420411960bbf2a1c7a48070d92e1
Muskan-Sachdeva <sachdevamuskan666@gmail.com>
2025-12-08 09:08:01 +0530

    Update genome_fraction_quast.R

--- genome_fraction_quast.R
+++ genome_fraction_quast.R
@@ -9,18 +9,18 @@ df <- data.frame(
   GenomeFraction = genome_fraction
 )
 
-# Plot
+
 p <- ggplot(df, aes(x = Genotype, y = GenomeFraction)) +
-  geom_bar(stat = "identity", fill = "steelblue", color = "black", width = 0.4) +
-  geom_text(aes(label = round(GenomeFraction,1)), vjust = -0.5, size = 3.5) +
+  geom_bar(stat = "identity", fill = "#2B687C", width = 0.5) +
+  geom_text(aes(label = round(GenomeFraction, 1)),
+            vjust = -0.5, size = 3.5) +
+  scale_y_continuous(limits = c(0, 90), breaks = seq(0, 100, 25)) +
+  expand_limits(y = max(df$GenomeFraction) + 5) + 
   labs(title = "Genome Fraction (%) by Genotype",
        x = "Genotype",
        y = "Genome Fraction (%)") +
-  theme_minimal(base_size = 13) +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  theme_classic(base_size = 14) +   # ★ WHITE BACKGROUND
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, face = "bold"))
 
 print(p)
-
-# Optional: save plot
-# ggsave("genome_fraction_barplot.png", p, width = 8, height = 5, dpi = 300)
 
