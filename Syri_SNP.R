a05b567062a8c2b5467e3e7c7b04084af3a4d405
Muskan-Sachdeva <sachdevamuskan666@gmail.com>
2025-12-08 09:08:07 +0530

    Update Syri_SNP.R

--- Syri_SNP.R
+++ Syri_SNP.R
@@ -3,12 +3,12 @@ library(ggplot2)
 library(scales)
 
 df <- data.frame(
-  Genotype = c("A", "B", "C", "D", "E"),
+  Genotype = c("HY3c", "MC50", "MC70", "ICPL-13271", "ICPL-87"),
   SNPs = c(451472, 400630, 445084, 603577, 513541)
 )
 
 ggplot(df, aes(x = Genotype, y = SNPs)) +
-  geom_bar(stat = "identity", width = 0.5, fill = "Steelblue") +   # narrower bars + neutral color
+  geom_bar(stat = "identity", width = 0.5, fill = "#2B687C") +   # narrower bars + neutral color
   scale_y_continuous(labels = comma) +                         # readable y-axis (with commas)
   labs(title = "SNP Count Across Genotypes",
        x = "Genotype",
