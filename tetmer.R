ae13a45a5200ad3c3b44ebef7e55fcb0b0ad4455
Muskan-Sachdeva <sachdevamuskan666@gmail.com>
2026-04-20 13:33:09 +0100

    Create tetmer.R

---
+++ tetmer.R
@@ -0,0 +1,9 @@
+spill <- read.spectrum("C:/Users/MUSKAN/Downloads/ONT_NSPP_3C_k21_histogram.txt",
+                       k=21
+                       
+                       )
+
+spill
+tetmer(spill)
+
+??Tetmer
